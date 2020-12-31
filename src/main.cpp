aa3c5555071d5a632d19ea6989f60bc5a1af2292
Matt Maher <matt@maher-family.net>
2020-12-30 17:18:29 -0800

    we have a light switch

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,22 @@
 
 #define ONBOARDLED 13
 #define LED 12
+#define SWITCH1 2
+#define ON 1
+#define OFF 0
+
 // changes
 void setup() {
     pinMode(ONBOARDLED, OUTPUT);
     pinMode(LED, OUTPUT);
+    pinMode(SWITCH1, INPUT);
+    // Serial.begin(9600);
 }
 void loop() {
-    blink(ONBOARDLED, 500);
-    blink(LED, 500);
+    // Serial.println(digitalRead(SWITCH1));
+    if (digitalRead(SWITCH1) == ON) {
+        digitalWrite(LED, ON);
+    } else {
+        digitalWrite(LED, OFF);
+    }
 }
