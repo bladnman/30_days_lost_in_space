7603f64392ee14d3d8d1cb95efd00754a5374ceb
Matt Maher <matt@maher-family.net>
2021-01-06 18:07:07 -0800

    brand new day - light sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,28 @@
-
 #include <Arduino.h>
 
-#include "blink.h"
+int onboardLED = 13;
+int sensorPin = A0;
+int sensorValue = 0;
 
-#define ONBOARDLED 13
-#define LED10 10
-#define LED11 11
-#define LED12 12
-#define SWITCH1 2
-#define SWITCH2 3
-#define SWITCH3 4
-#define ON 1
-#define OFF 0
-int outputs[] = {ONBOARDLED, LED10, LED11, LED12};
-int inputs[] = {SWITCH1, SWITCH2, SWITCH3};
+int minLight = 0;
+int maxLight = 700;
+int maxDelayMs = 1000;
+
 void setup() {
-    for (int item : outputs) pinMode(item, OUTPUT);
-    for (int item : inputs) pinMode(item, INPUT);
+    Serial.begin(9600);
+    pinMode(onboardLED, OUTPUT);
 }
 void loop() {
-    digitalWrite(LED10, digitalRead(SWITCH1) == ON);
-    digitalWrite(LED11, digitalRead(SWITCH2) == ON);
-    digitalWrite(LED12, digitalRead(SWITCH3) == ON);
+    sensorValue = analogRead(sensorPin);
+    Serial.println(sensorValue);
+
+    float lightPerc = (float)(maxLight - sensorValue) / (float)(maxLight - minLight);
+    float delayFloat = maxDelayMs * lightPerc;
+    int delayMs = (int)delayFloat;
+
+    digitalWrite(onboardLED, HIGH);
+    delay(delayMs);
+    digitalWrite(onboardLED, LOW);
+    delay(delayMs);
+    Serial.println(delayMs);
 }
