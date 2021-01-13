0718843c59d01969eada26bad15dbb577a3fd7dd
Matt Maher <matt@maher-family.net>
2021-01-12 17:15:03 -0800

    we have buzzing

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <Keypad.h>
 
-int lightSensor = A0;
+int buzzer = 10;
 
 const byte ROWS = 4;
 const byte COLS = 4;
@@ -18,15 +18,34 @@ char buttons[ROWS][COLS] = {
 byte rowPins[ROWS] = {5, 4, 3, 2};
 byte colPins[COLS] = {6, 7, 8, 9};
 
+int tones[ROWS][COLS] = {
+    {31, 93, 147, 208},
+    {247, 311, 370, 440},
+    {523, 587, 698, 880},
+    {1397, 2637, 3729, 4978}};
 Keypad customKeypad = Keypad(makeKeymap(buttons), rowPins, colPins, ROWS, COLS);
 
 void setup() {
     Serial.begin(9600);
 }
 void loop() {
+    int toneFreq = 0;
+
     char customKey = customKeypad.getKey();
 
     if (customKey) {  // if a button is pressed
-        Serial.println(customKey);
+        Serial.print(customKey);
+        Serial.print(": ");
+        for (byte j = 0; j < ROWS; j++) {
+            for (byte i = 0; i < COLS; i++) {
+                if (customKey == buttons[j][i]) {
+                    toneFreq = tones[j][i];
+                    break;
+                }
+            }
+        }
+        tone(buzzer, toneFreq, 250);
+        delay(250);
+        noTone(buzzer);
     }
 }
